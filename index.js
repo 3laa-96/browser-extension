50a49a27f98c59c60b6f0a64bd7770ae9aa4cc75
3laa-96 <3laa.xvx@gmail.com>
2022-12-21 13:43:42 +0200

    v1.0

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-function save() {
-  console.log("button clicked");
-}
+// Defined variables
+let myLeads = [];
+const savedText = document.getElementById("saved-text");
+const inputEl = document.getElementById("input-el");
+const saveBtn = document.getElementById("save-btn");
+saveBtn.addEventListener("click", function save() {
+  myLeads.push(inputEl.value);
+  myLeads.forEach(function () {
+    const element = document.createElement("li");
+    element.innerText = myLeads;
+    savedText.appendChild(element);
+    myLeads.shift();
+  });
+});
