216aa3c320590d80726c231e546ecbb79180dfe3
3laa-96 <3laa.xvx@gmail.com>
2022-12-21 14:25:42 +0200

    v1.2

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const savedText = document.getElementById("saved-text");
 const inputEl = document.getElementById("input-el");
 const saveBtn = document.getElementById("save-btn");
 saveBtn.addEventListener("click", function save() {
-  myLeads.push(inputEl.value);
-  myLeads.forEach(function () {
-    const element = document.createElement("li");
-    element.innerText = myLeads;
-    savedText.appendChild(element);
-    myLeads.shift();
-  });
+  if (inputEl.value === "") {
+    alert("No input detected !");
+  } else {
+    myLeads.push(inputEl.value);
+    myLeads.forEach(function () {
+      const element = document.createElement("li");
+      element.innerText = myLeads;
+      savedText.appendChild(element);
+      myLeads.shift();
+    });
+  }
+
+  document.getElementById("input-el").value = "";
 });
