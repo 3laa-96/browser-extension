c40b71bdd38abd15e1b0ba1e78530b14df719fc1
3laa-96 <3laa.xvx@gmail.com>
2022-12-23 13:17:52 +0200

    v2.0

--- index.js
+++ index.js
@@ -3,22 +3,25 @@ let myLeads = [];
 const savedText = document.getElementById("saved-text");
 const inputEl = document.getElementById("input-el");
 const saveBtn = document.getElementById("save-btn");
+
+// Save Button
 saveBtn.addEventListener("click", function save() {
+  myLeads.push(inputEl.value);
   render();
-  document.getElementById("input-el").value = "";
+  inputEl.value = "";
 });
 
 // Render text function
 function render() {
   if (inputEl.value === "") {
-    alert("No input detected !");
+    // inputEl.placeholder = "No input detected !"; WIP
+    alert("No input detected");
   } else {
-    myLeads.push(inputEl.value);
-    myLeads.forEach(function () {
-      const element = document.createElement("li");
-      element.innerText = myLeads;
-      savedText.appendChild(element);
-      myLeads.shift();
+    myLeads.forEach(function (link) {
+      const li = document.createElement("li");
+      li.innerHTML = `<a href="${link}" target="_blank">${link}</a>`;
+      savedText.appendChild(li);
+      myLeads.pop();
     });
   }
 }
