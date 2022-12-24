fc1ff2efbceddc97aef890dc79f284a3e5017b9d
3laa-96 <3laa.xvx@gmail.com>
2022-12-24 13:47:32 +0200

    v2.1

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const saveBtn = document.getElementById("save-btn");
 // Save Button
 saveBtn.addEventListener("click", function save() {
   myLeads.push(inputEl.value);
+  localStorage.setItem("savedLinks", JSON.stringify(myLeads));
   render();
   inputEl.value = "";
+  // localStorage.getItem("savedLinks", JSON.parse(myLeads));
 });
 
 // Render text function
@@ -17,11 +19,14 @@ function render() {
     // inputEl.placeholder = "No input detected !"; WIP
     alert("No input detected");
   } else {
-    myLeads.forEach(function (link) {
+    myLeads.forEach(function (link, index) {
+      myLeads[index] = myLeads; //WIP
       const li = document.createElement("li");
-      li.innerHTML = `<a href="${link}" target="_blank">${link}</a>`;
+      li.innerHTML = `<a href="https://${link}" target="_blank">${link}</a>`;
       savedText.appendChild(li);
-      myLeads.pop();
+      // myLeads.pop();
     });
   }
 }
+
+// 6:10:10
